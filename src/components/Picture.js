b4b4c602306f12353e7bc98b39c004617504f6e2
F54101056 <40814498+phi622968143@users.noreply.github.com>
2024-01-11 12:21:11 +0800

    Update Picture.js
    img medium

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -4,7 +4,7 @@ const Picture = ({data}) => {//data from homepage
   return (
    <div className="picture">
     <div className="imageContainer">
-      <a href={data.src.large} target={"_blank"}><img src={data.src.large} alt="" /></a>
+      <a href={data.src.large} target={"_blank"}><img src={data.src.medium} alt="" /></a>
     </div>
    </div>
   );
